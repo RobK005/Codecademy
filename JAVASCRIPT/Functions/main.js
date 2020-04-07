bd6252b8cdb8c31e8cb15336aba6cfe95173c7fd
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-07 16:07:10 +0200

    Default Parameters

--- JAVASCRIPT/Functions/main.js
+++ JAVASCRIPT/Functions/main.js
@@ -18,3 +18,14 @@ function sayThanks(name) {
 
 sayThanks('Cole');
 
+console.log('------------------------');
+console.log('---Default Parameters---');
+
+function makeShoppingList(item1='milk', item2='bread', item3='eggs') {
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+};
+
+makeShoppingList('butter');
+
