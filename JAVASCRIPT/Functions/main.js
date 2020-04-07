68d454b159b1ebb1ef7f1a8f927b81512c429b8c
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-07 16:02:45 +0200

    Parameters and Arguments

--- JAVASCRIPT/Functions/main.js
+++ JAVASCRIPT/Functions/main.js
@@ -1,7 +1,20 @@
-function sayThanks() {
-    console.log('Thank you for your purchase! We appreciate your business.')
-}
+console.log('------------------------');
+console.log('---Calling a Function---');
 
-sayThanks()
-sayThanks()
-sayThanks()
+function sayThanks() {
+    console.log('Thank you for your purchase! We appreciate your business.');
+};
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+console.log('------------------------------');
+console.log('---Parameters and Arguments---');
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name +'! We appreciate your business.');
+};
+
+sayThanks('Cole');
+
