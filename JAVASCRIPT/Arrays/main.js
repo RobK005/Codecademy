57076d5a900968ec88103e51d17bfd01744909f6
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-11 05:54:09 +0200

    Adding Arrays files

---
+++ JAVASCRIPT/Arrays/main.js
@@ -0,0 +1,9 @@
+console.log('------------------------');
+console.log('---Accessing Elements---');
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+var listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[1]);
+console.log(famousSayings[2]);
