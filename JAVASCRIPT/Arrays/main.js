366862d8066fa00fd9b07ee76a17349a8cf13cc1
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-12 06:48:36 +0200

    Update Elements

--- JAVASCRIPT/Arrays/main.js
+++ JAVASCRIPT/Arrays/main.js
@@ -7,3 +7,11 @@ var listItem = famousSayings[0];
 console.log(listItem);
 console.log(famousSayings[1]);
 console.log(famousSayings[2]);
+
+console.log('---------------------');
+console.log('---Update Elements---');
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'butter'
+
+console.log(groceryList);
