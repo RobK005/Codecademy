11dd306dd66a0f92a5a592774cc9aa6589e96e39
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-16 19:37:13 +0200

    Looping through Arrays

--- JAVASCRIPT/Loops/main.js
+++ JAVASCRIPT/Loops/main.js
@@ -11,3 +11,12 @@ console.log('---Looping in Reverse---');
 for (let counter = 3; counter >= 0; counter--) {
   console.log(counter);
 };
+
+console.log('----------------------------');
+console.log('---Looping through Arrays---');
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log('I would love to visit '+ vacationSpots[i]);
+};
