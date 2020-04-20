39af83b7ce49d385ced005383d7de994734ba97e
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-20 07:21:09 +0200

    Adding Iterators files

---
+++ JAVASCRIPT/Iterators/main.js
@@ -0,0 +1,7 @@
+console.log('---------------------------');
+console.log('---The .forEach() Method---');
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(groceryItem => console.log('I want to eat a '+ groceryItem));
