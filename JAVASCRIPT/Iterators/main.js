48ef80dff2bdaf20a34b424b0a6ab25aba40a938
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-21 06:01:18 +0200

    The .map() Method

--- JAVASCRIPT/Iterators/main.js
+++ JAVASCRIPT/Iterators/main.js
@@ -5,3 +5,20 @@ const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
 fruits.forEach(groceryItem => console.log('I want to eat a '+ groceryItem));
+
+console.log('-----------------------');
+console.log('---The .map() Method---');
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// New secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(' '));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// New smallNumbers array below
+const smallNumbers = bigNumbers.map(number => number / 100);
+
+console.log(smallNumbers);
