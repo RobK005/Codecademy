d724b3bebe2ad0175d96346ef0fb7fbba7c874c4
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-24 06:15:34 +0200

    Bracket Notation

--- JAVASCRIPT/Objects/main.js
+++ JAVASCRIPT/Objects/main.js
@@ -5,13 +5,21 @@ let spaceship = {
   homePlanet: 'Earth',
   color: 'silver',
   'Fuel Type': 'Turbo Fuel',
+  'Active Mission' : true,
   numCrew: 5,
   flightPath: ['Venus', 'Mars', 'Saturn']
 };
 
 const crewCount = spaceship.numCrew;
 const planetArray =  spaceship.flightPath;
-const shipFuel = spaceship['Fuel Type'];
 
 console.log(planetArray);
+
+console.log('----------------------');
+console.log('---Bracket Notation---');
+
+const shipFuel = spaceship['Fuel Type'];
+let propName =  'Active Mission';
+
 console.log(shipFuel);
+console.log(spaceship[propName])
