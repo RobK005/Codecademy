c870512b9c2fa349068dd7702f59ded35d332fe6
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-26 08:13:51 +0200

    Adding Advanced Objects files

---
+++ JAVASCRIPT/Advanced Objects/main.js
@@ -0,0 +1,12 @@
+console.log('-----------------------');
+console.log('---The .this Keyword---');
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}. `
+  }
+};
+
+console.log(robot.provideInfo());
