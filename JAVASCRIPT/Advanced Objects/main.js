12f6a5c992167c026e2accbce67c1550aa84d9e3
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-27 16:18:31 +0200

    Getters

--- JAVASCRIPT/Advanced Objects/main.js
+++ JAVASCRIPT/Advanced Objects/main.js
@@ -32,3 +32,20 @@ robot2.recharge();
 
 // This method doesn't work correctly because of ARROW function. In this case .this is refering to the GLOBAL object, not robot2. It's better to avoid those kind of functions inside objects.
 robot2.faultyRecharge();
+
+console.log('-------------');
+console.log('---Getters---');
+
+const robot3 = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === 'number') {
+      return `My current energy level is ${this._energyLevel}`
+    } else {
+      return `System malfunction: cannot retrieve energy level`
+    }
+  },
+};
+
+console.log(robot3.energyLevel);
