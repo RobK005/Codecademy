77d5a8fec6fde5ed2b84423ca02c2b4a28b5c883
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-04-27 16:32:27 +0200

    Setters

--- JAVASCRIPT/Advanced Objects/main.js
+++ JAVASCRIPT/Advanced Objects/main.js
@@ -49,3 +49,29 @@ const robot3 = {
 };
 
 console.log(robot3.energyLevel);
+
+console.log('-------------');
+console.log('---Setters---');
+
+const robot4 = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if(typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0');
+    }
+  }
+};
+
+robot4.numOfSensors = 100;
+console.log(robot4.numOfSensors);
