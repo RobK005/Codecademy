986a232fa2810c7a38e2a9c946b1363a589183a8
Robert Kwaśnicki <r.kwasnicki005@gmail.com>
2020-05-03 05:39:14 +0200

    Method Calls

--- JAVASCRIPT/Classes/main.js
+++ JAVASCRIPT/Classes/main.js
@@ -1,13 +1,49 @@
 console.log('---------------------------');
 console.log('---Constructor, Instance---');
 
-class Surgeon {
+class SurgeonConstruct {
   constructor(name, department) {
     this.name = name;
     this.department = department;
   };
 };
 
+const surgeonJohn = new SurgeonConstruct('John', 'Cardiovascular');
+const surgeonEmma = new SurgeonConstruct('Emma', 'Orthopedics');
+console.log(surgeonJohn);
+
+console.log('------------------');
+console.log('---Method Calls---');
+
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+    return this._remainingVacationDays;
+  }
+};
+
 const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
 const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-console.log(surgeonDurant);
+console.log(surgeonDurant.takeVacationDays(5));
+
+console.log(surgeonCurry.name);
+surgeonCurry.takeVacationDays(3);
+console.log(surgeonCurry.remainingVacationDays);
